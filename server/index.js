b7f9e71f7189b74a70f916a5c9aa8a79364f11ff
Ankrath <ankrathgg@gmail.com>
2025-01-11 21:52:50 +0000

    Added regionMappings to a separate file

--- server/index.js
+++ server/index.js
@@ -3,77 +3,12 @@ import axios from 'axios';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import { regionMappings } from './regions.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
-
-const regionMappings = {
-  BR: {
-    platform: 'br1',
-    regional: 'americas',
-  },
-  EUN: {
-    platform: 'eun1',
-    regional: 'europe',
-  },
-  EUW: {
-    platform: 'euw1',
-    regional: 'europe',
-  },
-  JP: {
-    platform: 'jp1',
-    regional: 'asia',
-  },
-  KR: {
-    platform: 'kr',
-    regional: 'asia',
-  },
-  LAN: {
-    platform: 'la1',
-    regional: 'americas',
-  },
-  LAS: {
-    platform: 'la2',
-    regional: 'americas',
-  },
-  NA: {
-    platform: 'na1',
-    regional: 'americas',
-  },
-  OCE: {
-    platform: 'oc1',
-    regional: 'sea',
-  },
-  TR: {
-    platform: 'tr1',
-    regional: 'europe',
-  },
-  RU: {
-    platform: 'ru',
-    regional: 'europe',
-  },
-  PH: {
-    platform: 'ph2',
-    regional: 'sea',
-  },
-  SG: {
-    platform: 'sg2',
-    regional: 'sea',
-  },
-  TH: {
-    platform: 'th2',
-    regional: 'sea',
-  },
-  TW: {
-    platform: 'tw2',
-    regional: 'asia',
-  },
-  VN: {
-    platform: 'vn2',
-    regional: 'sea',
-  },
-};
 
 app.get('/summoner/:name/:tag/:region', async (req, res) => {
   try {
