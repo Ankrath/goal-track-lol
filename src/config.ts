8a7a392c7315080bf1fc26e04cecb0ecae6ce3f1
Ankrath <ankrathgg@gmail.com>
2025-01-17 16:34:19 +0000

    Debugging prod env issue

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 const config = {
-  apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3001',
+  apiUrl: import.meta.env.VITE_API_URL,
 };
 
 console.log('Current environment:', import.meta.env.MODE);
 console.log('API URL being used:', config.apiUrl);
+console.log('Raw env variable:', import.meta.env.VITE_API_URL);
 
 export default config;
