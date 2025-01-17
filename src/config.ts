43dfec2cc3cf47a221fd025f84812264a3444426
Ankrath <ankrathgg@gmail.com>
2025-01-17 16:41:25 +0000

    Cleaning up debugging for prod env issues

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,4 @@ const config = {
   apiUrl: import.meta.env.VITE_API_URL,
 };
 
-console.log('Current environment:', import.meta.env.MODE);
-console.log('API URL being used:', config.apiUrl);
-console.log('Raw env variable:', import.meta.env.VITE_API_URL);
-
 export default config;
